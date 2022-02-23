db02dbaa36dbbf9c0de025e7dfc198f0639bea73
nachooantonuccio <nachoantonuccio@gmail.com>
2022-02-23 13:53:57 -0300

    se agregan validaciones

--- calculadora.js
+++ calculadora.js
@@ -1,11 +1,18 @@
 function calcularIMC(){
 	var altura = document.getElementById("altura");
 	var peso = document.getElementById("peso");
-	var IMC = (peso.value / (altura.value ** 2));
-	document.getElementById("resultado").innerHTML = IMC;
-	document.getElementById("resultado").style.visivility = "visible";
+	if(altura.value == "" || peso.value == ""){
+		alert("Debe ingresar el peso y la altura");
+	}
+	else{
+		var IMC = (peso.value / (altura.value ** 2));
+		document.getElementById("resultado").innerHTML = IMC.toFixed(1);
+		document.getElementById("resultado").style.visivility = "visible";
+	}
 }
 
 function limpiar(){
 		document.getElementById("resultado").innerHTML = "";
+		document.getElementById("altura").value = "";
+		document.getElementById("peso").value = "";
 }
