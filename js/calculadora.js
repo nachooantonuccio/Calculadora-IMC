321147151365639e16cbec8388378b42dbea9fa9
nachooantonuccio <nachoantonuccio@gmail.com>
2022-03-07 12:40:30 -0300

    algunos cambios

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,11 +1,13 @@
 function calcularIMC(){
 	var altura = document.getElementById("altura");
 	var peso = document.getElementById("peso");
-	if(altura.value == "" || peso.value == ""){
+	if (altura.value == "" || peso.value == "") {
 		swal("Atención","Debe ingresar altura y peso","warning");
-	}
-	else{
+	} else {
 		var IMC = (peso.value / (altura.value ** 2));
+		if(isNaN(IMC)){
+			swal("Atención","Debe ingresar altura y peso validos. Intentaste modificar el html","warning");
+		}
 		document.getElementById("resultado").innerHTML = IMC.toFixed(1);
 		document.getElementById("resultado").style.visivility = "visible";
 	}
@@ -16,3 +18,18 @@ function limpiar(){
 		document.getElementById("altura").value = "";
 		document.getElementById("peso").value = "";
 }
+
+botonCalcular = document.getElementById('botonCalcular');
+botonCalcular.addEventListener('click', () => {
+
+	calcularIMC();
+
+},false);
+
+
+botonLimpiar = document.getElementById('botonLimpiar');
+botonLimpiar.addEventListener('click',() => {
+
+	limpiar();
+
+},false);
